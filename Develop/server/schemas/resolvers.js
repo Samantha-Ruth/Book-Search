e495cb00d3d9190c0eeacb00944e846e8dacaccf
Samantha-Ruth <sam.haberman@gmail.com>
2022-09-10 09:33:43 -0600

    solved typo in code that was returning 'null' instead of user properties

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -24,7 +24,7 @@ const resolvers = {
             // const token = signToken(user);
       
             // return { token, user };
-            return User;
+            return user;
           },
 
           // user login
